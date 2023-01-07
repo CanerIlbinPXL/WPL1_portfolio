04c6444420de6587353645794a5c8b30006f7c8b
Caner Ilbin <caner.ilbin@student.pxl.be>
2023-01-07 22:09:35 +0100

    Eerste commit - Alle tabs zijn af - Assets toegevoegd

---
+++ CV.js
@@ -0,0 +1,12 @@
+var text = 0;
+var actualtext = 'Ik ben Caner Ilbin, een 21-jarige student programmeren aan de Hogeschool PXL. Ik ben een gedreven en gemotiveerde programmeur die resultaatgericht en probleemoplossend te werk gaat. Naast programmeren houd ik me graag in goede fysieke conditie in de sportschool, daarnaast game ik ook graag.'+
+    'Naast programmeren wil ik me ook verdiepen in het maken van ontwerpen en het animeren daarvan. Professionele videobewerking spreekt me ook erg aan.';
+var speed = 50;
+
+function AutoTypetxt() {
+if (text < actualtext.length) {
+    document.getElementById("generate_text").innerHTML += actualtext.charAt(text);
+    text++;
+    setTimeout(AutoTypetxt, speed);
+}
+}
