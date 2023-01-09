863c88508c234dbb8dab85d5613f528e8bfb2da4
Caner Ilbin <caner.ilbin@student.pxl.be>
2023-01-09 17:12:36 +0100

    Eerste commit - Alle tabs zijn af - Assets toegevoegd - Responsive gemaakt

--- CV.js
+++ CV.js
@@ -1,6 +1,6 @@
 var text = 0;
-var actualtext = 'Ik ben Caner Ilbin, een 22-jarige student programmeren aan de Hogeschool PXL. Ik ben een gedreven en gemotiveerde programmeur die resultaatgericht en probleemoplossend te werk gaat. Naast programmeren houd ik me graag in goede fysieke conditie in de sportschool, daarnaast game ik ook graag.'+
-    'Naast programmeren wil ik me ook verdiepen in het maken van ontwerpen en het animeren daarvan. Professionele videobewerking spreekt me ook erg aan.';
+var actualtext = 'Ik ben Caner Ilbin, een 22-jarige student programmeren aan de Hogeschool PXL. Ik ben een gedreven en gemotiveerde programmeur die resultaatgericht en probleemoplossend te werk gaat. Ik doe aan fitness en game ook graag.'+
+    ' Naast programmeren wil ik me ook verdiepen in het maken van ontwerpen en het animeren daarvan. Professionele videobewerking spreekt me ook erg aan.';
 var speed = 50;
 
 function AutoTypetxt() {
